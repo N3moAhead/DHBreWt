b3e5a2402f17b2a84a2c5faea9df263f154ae334
Lukas Heberling <lukaselia.heberling@gmail.com>
2023-11-27 12:15:49 +0100

    Backend: Added a documentation page for the api routes

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,11 @@
-from flask import Flask;
+from flask import Flask
+from flask import render_template
 
-app = Flask(__name__);
+app = Flask(__name__)
 
-@app.route('/')
-def home():
-  return "Hello, DHBreWt!";
+@app.route('/docs')
+def docs():
+    return render_template("docs.html")
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
